1fe24f119e1e0ba3864abf6e598b54f98ed50358
akhandps <akhandps2007@gmail.com>
2026-03-19 01:38:38 +0530

    update error

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,4 +1,31 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+// import axios from "axios";
+// import { useDispatch } from "react-redux";
+// import { setOtherUsers } from '../redux/userSlice';
+// import { BASE_URL } from '..';
+
+// const useGetOtherUsers = () => {
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         const fetchOtherUsers = async () => {
+//             try {
+//                 axios.defaults.withCredentials = true;
+//                 const res = await axios.get(`${BASE_URL}/api/v1/user`);
+//                 // store
+//                 console.log("other users -> ",res);
+//                 dispatch(setOtherUsers(res.data));
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         }
+//         fetchOtherUsers();
+//     }, [])
+
+// }
+
+// export default useGetOtherUsers
+import { useEffect } from 'react';
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setOtherUsers } from '../redux/userSlice';
@@ -12,7 +39,6 @@ const useGetOtherUsers = () => {
             try {
                 axios.defaults.withCredentials = true;
                 const res = await axios.get(`${BASE_URL}/api/v1/user`);
-                // store
                 console.log("other users -> ",res);
                 dispatch(setOtherUsers(res.data));
             } catch (error) {
@@ -20,8 +46,7 @@ const useGetOtherUsers = () => {
             }
         }
         fetchOtherUsers();
-    }, [])
-
+    }, [dispatch])
 }
 
 export default useGetOtherUsers
