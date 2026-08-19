bba2343313bc218e8c86bf2d0aebc27b69f8a547
akhandps <akhandps2007@gmail.com>
2026-08-19 23:29:40 +0530

    Fix ESLint warnings causing Vercel deployment failures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from './components/HomePage';
 import Login from './components/Login';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import {useSelector,useDispatch} from "react-redux";
 import io from "socket.io-client";
 import { setSocket } from './redux/socketSlice';
@@ -51,6 +51,7 @@ function App() {
       }
     }
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[authUser]);
 
   return (
